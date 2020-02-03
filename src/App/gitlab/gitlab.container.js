185da8bd9d096e765acefe7557aeb81cc47dca0c
SATaylor <engelstern7@yahoo.com>
2020-02-03 14:11:42 -0800

    adding the fetching of the list setup

--- src/App/gitlab/gitlab.container.js
+++ src/App/gitlab/gitlab.container.js
@@ -1,9 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import PRList from "./components/prList";
 import Divider from "@material-ui/core/Divider";
 import { objectSort } from "./../../utils/objectSort";
+import APIResource from "./../../resource/api.resource";
+import { getUrlParameter } from "./../../utils/url";
+
+const resource = new APIResource();
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -34,6 +38,12 @@ const initialData = [
   }
 ];
 
+const callResource = ({ url, version, type, perPage, privateToken }) => {
+  return resource.fetch(
+    `${url}/${version}/${type}?per_page=${perPage}&private_token=${privateToken}`
+  );
+};
+
 const GitlabContainer = () => {
   const classes = useStyles();
   const [list, setList] = useState(initialData);
@@ -42,6 +52,43 @@ const GitlabContainer = () => {
     const sortedList = objectSort(list, sortKey);
     setList(sortedList);
   };
+
+  useEffect(() => {
+    const token = getUrlParameter("private_token");
+    const newList = [];
+    // this looks so ugly... need to clean up
+    callResource({
+      url: process.env.REACT_APP_GITLAB_URL,
+      version: process.env.REACT_APP_GITLAB_API_VERSION,
+      type: "merge_requests",
+      perPage: 50,
+      privateToken: token
+    }).then(mrData => {
+      callResource({
+        url: process.env.REACT_APP_GITLAB_URL,
+        version: process.env.REACT_APP_GITLAB_API_VERSION,
+        type: "projects",
+        perPage: 1000,
+        privateToken: token
+      }).then(prData => {
+        let projectMap = new Map();
+        prData.forEach(pr => {
+          projectMap.set(pr.id, pr.name_with_namespace);
+        });
+        mrData.forEach(mr => {
+          newList.push({
+            project: projectMap.get(mr.project_id),
+            title: mr.title,
+            link: mr.web_url,
+            author: mr.author.name
+          });
+        });
+
+        // when the two loops are done then set the list
+        setList(newList);
+      });
+    });
+  }, []);
 
   return (
     <div className={classes.root}>
